b2149572c351938032e84ea26dec6bd5bfbba488
Lucas Lira <lucasclira@outlook.com>
2023-11-23 19:13:17 -0300

    creat new usecase for verify by protocol

---
+++ project/core/usecase/scheduler/verify_scheduler_by_protocol/verify_scheduler_by_protocol_usecase.py
@@ -0,0 +1,15 @@
+from project.core.ports.scheduler_repository_port import SchedulerRepositoryPort
+from infrastructure.persistence.repository.postgres_scheduler_repository_adapter import (
+    PostgresSchedulerRepositoryAdapter,
+)
+
+class VerifySchedulerByProtocolUseCase(UseCaseInterface):
+    def __init__(self, db: Session):
+        self.repository = PostgresSchedulerRepositoryAdapter(db)
+
+    def execute(self, protocol: str):
+        scheduler = self.repository.get_by_protocol(protocol)
+        if scheduler is None:
+            return 'error'
+        else:
+            return scheduler
