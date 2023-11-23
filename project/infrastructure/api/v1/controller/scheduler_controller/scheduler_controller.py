d0cd4375ed81d1bcb989959fca229fa1497cee47
Lucas Lira <lucasclira@outlook.com>
2023-11-23 19:48:03 -0300

    create router fo verify-status

--- project/infrastructure/api/v1/controller/scheduler_controller/scheduler_controller.py
+++ project/infrastructure/api/v1/controller/scheduler_controller/scheduler_controller.py
@@ -9,6 +9,9 @@ from core.usecase.scheduler.request_scheduler.dto import (
 from core.usecase.scheduler.request_scheduler.request_scheduler_usecase import (
     RequestSchedulerUseCase,
 )
+from project.core.usecase.scheduler.verify_scheduler_by_protocol.verify_scheduler_by_protocol_usecase import (
+    VerifySchedulerByProtocolUseCase,
+)
 from infrastructure.api.v1.controller.dependencies.token import get_user_by_token
 from infrastructure.persistence.repository.session import get_db
 
@@ -24,3 +27,13 @@ def request_scheduler(
     scheduler_input.set_user(current_user)
     usecase = RequestSchedulerUseCase(db)
     return usecase.execute(scheduler_input)
+
+
+@router.post('/verify-status', response_model=RequestSchedulerOutput)
+def verify_status(
+        scheduler_input: RequestSchedulerInput,
+        db: Session = Depends(get_db)
+) -> RequestSchedulerOutput:
+    protocol = scheduler_input.protocol
+    scheduler = VerifySchedulerByProtocolUseCase(db)
+    return scheduler.execute(protocol)
