963a53b174148422a95f467b02e955e6530123a9
Lucas Lira <lucasclira@outlook.com>
2023-12-01 18:22:37 -0300

    create router of get-all-schedulers-by-user

--- project/infrastructure/api/v1/controller/scheduler_controller/scheduler_controller.py
+++ project/infrastructure/api/v1/controller/scheduler_controller/scheduler_controller.py
@@ -11,6 +11,7 @@ from core.usecase.scheduler.approve_scheduler.approve_scheduler_usecase import (
 )
 from core.usecase.scheduler.approve_scheduler.dto import ApproveSchedulerOutput
 from core.usecase.scheduler.get_all_scheduler.dto import GetAllSchedulerOutput, FiltersInput
+from core.usecase.scheduler.get_all_scheduler_by_user.dto import GetAllSchedulerByUserOutput
 from core.usecase.scheduler.get_all_scheduler.get_all_scheduler_usecase import (
     GetAllSchedulerUseCase,
 )
@@ -104,5 +105,21 @@ def get_all_scheduler(
     try:
         schedulers = usecase.execute(filters)
         return GetAllSchedulerOutput.from_domain(schedulers)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/{current_user}/all", response_model=List[GetAllSchedulerByUserOutput])
+def get_all_scheduler_by_user(
+    current_user: User = Depends(get_user_by_token),
+    db: Session = Depends(get_db),
+):
+    usecase = GetAllSchedulerUseCase(db)
+
+    try:
+        schedulers = usecase.execute(current_user.id)
+        return GetAllSchedulerByUserOutput.from_domain(schedulers)
+    except SchedulerNotFoundError as s:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(s))
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
